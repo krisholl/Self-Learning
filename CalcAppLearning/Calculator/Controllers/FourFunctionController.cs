7b1146116b82d1b993b0eba0a4c8da43d840e760
krisholl <krisholl2022@gmail.com>
2022-09-28 14:28:32 -0400

    Finished back end for 4-function bit
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ CalcAppLearning/Calculator/Controllers/FourFunctionController.cs
@@ -0,0 +1,39 @@
+﻿using Service;
+using Microsoft.AspNetCore.Mvc;
+namespace Calculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FourFunctionController:ControllerBase
+    {
+        private readonly FourFunction _fourFunction;
+        public FourFunctionController(FourFunction fourFunction)
+        {
+            _fourFunction = fourFunction;
+        }
+        [HttpGet()]
+        [Route("/Add")]
+        public ActionResult<double> Add(double var1, double var2)
+        {
+            return Ok(_fourFunction.Add(var1, var2));
+        }
+        [HttpGet()]
+        [Route("/Subtract")]
+        public ActionResult<double> Subtract(double var1,double var2)
+        {
+            return Ok(_fourFunction.Add(var1,var2*-1));
+        }
+        [HttpGet()]
+        [Route("/Multiply")]
+        public ActionResult<double> Multiply(double var1,double var2)
+        {
+            return Ok(_fourFunction.Multiply(var1,var2));
+        }
+        [HttpGet()]
+        [Route("/Divide")]
+        public ActionResult<double> Divide(double var1,double var2)
+        {
+            return Ok(_fourFunction.Divide(var1,var2));
+        }
+    }
+}
---
+++ CalcAppLearning/Service/FourFunction.cs
@@ -0,0 +1,25 @@
+﻿namespace Service
+{
+    public class FourFunction
+    {
+        public FourFunction()
+        {
+
+        }
+
+        public double Add(double var1, double var2)
+        {
+            return var1 + var2;
+        }
+
+        public double Multiply(double var1, double var2)
+        {
+            return var1 * var2;
+        }
+
+        public double Divide(double var1,double var2)
+        {
+            return var1/ var2;
+        }
+    }
+}
